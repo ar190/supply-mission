5c7c01bf1ba1a2a2a45e9b5a4ce66a5e4cc7d9e3
ar190 <69691425+ar190@users.noreply.github.com>
2020-08-21 16:33:03 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,57 +1,61 @@
-
-
-var car,wall;
-
-
-var speed, weight; 
-
+var bullet,speed,weight;
+var wall,thickness;
 
 function setup() {
-  createCanvas(1600, 400);
-
-
-	speed=random(55,90)
-	weight=random(400,1500)
-
-
-	car=createSprite(50, 200, 50,50);   
-
-	car.velocityX = speed;
-
-	car.shapeColor=color(255);
-
-
-  
-  	wall=createSprite(1500,200, 60, height/2)
-  	wall.shapeColor=color(80,80,80)
+  createCanvas(1600,400);
+  bullet=createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,thickness,height/2);
+  bullet.velocityX=speed;
+  wall.shapeColor=(80,80,80);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
 }
-
 
 function draw() {
-  background(0);
+  background(255,255,255); 
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if(deformation>180){
+      bullet.shapeColor=color(255,0,0);
+    }
+  if(deformation<180 && deformation>100){
+    bullet.shapeColor=color(230,230,0);
+  }
+  if(deformation<100){
+     bullet.shapeColor=color(0,255,0);
+  }
+  }
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(g=damage>10){
+      wall.shapeColor=color=(255,0,0);
+    }
+    if(damage<10){
+wall.shaeColor=color(0,255,0);
+    }
+  
+  }
 
-  if(wall.x-car.x < (car.width+wall.width)/2)
-  {
-  	car.velocityX=0;
-  	var deformation=0.5 * weight * speed* speed/22509;
-	if(deformation>180)
-	{
-		car.shapeColor=color(255,0,0);
-	}
-
-	if(deformation<180 && deformation>100)
-	{
-		car.shapeColor=color(230,230,0);
-	}
-
-	if(deformation<100)
-	{
-		car.shapeColor=color(0,255,0);
-	}
-  }  
   
   drawSprites();
- 
 }
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge+lbullet.x+lbullet.width;
+  wallLeftEdge+lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
+
+
+
+
+
+
 
 
